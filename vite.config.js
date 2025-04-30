0a0324b377d611397771f324385e19680010f21c
Vishal Tambi <138616107+vishal-tambi@users.noreply.github.com>
2025-05-01 00:09:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://globetrotter-backend-31lk.onrender.com/',
+        target: 'https://globetrotter-backend-31lk.onrender.com',
         changeOrigin: true,
         secure: false
       }
