a0cc1c79086b1b62f65e9a8bdf4d0154bd9884c6
Vishal Tambi <tambivishal3@gmail.com>
2025-04-30 23:12:33 +0530

    connecting with backend

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://globetrotter-backend-31lk.onrender.com/',
         changeOrigin: true,
         secure: false
       }
